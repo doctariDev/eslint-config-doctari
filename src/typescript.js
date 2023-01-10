71358ff7d5ce3d849c19bc0a819d23668dadec8f
Andreas Moeller <andrmoel@gmail.com>
2023-01-10 09:26:49 +0100

    feat(typescript): add new rule no-unnecessary-type-assertion

--- src/typescript.js
+++ src/typescript.js
@@ -30,6 +30,7 @@ module.exports = {
         ],
         '@typescript-eslint/await-thenable': 'error',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'error',
         '@typescript-eslint/naming-convention': [
             'error',
             {
