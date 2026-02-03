4462fc2b8c2c66a9e4a4501ba6fe4eb09e18c558
Andreas Möller <andrmoel@gmail.com>
2026-02-03 14:55:47 +0100

    Revert "feat(): upgrade to ESLint 9 with flat config format (#5)"
    This reverts commit 131e4d566451a34846a311a52d3f3c2123e158be.

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import defaultConfig from './default.js';
-import reactConfig from './react.js';
-import typescriptConfig from './typescript.js';
-
-export {defaultConfig, reactConfig, typescriptConfig};
-export default [...defaultConfig, ...reactConfig, ...typescriptConfig];
